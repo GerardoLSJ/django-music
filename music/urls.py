73d30db5b427850d2c4a71e22f3b46a70dbe1c20
lsjgerardo <lsjgerardo@gmail.com>
2017-01-08 21:22:33 -0600

    returns h2

--- music/urls.py
+++ music/urls.py
@@ -1,6 +1,11 @@
 from django.conf.urls import url
 from . import views
 
+
 urlpatterns = [
-    url(r'^$', views.index, name='index')
+    # music/
+    url(r'^$', views.index, name='index'),
+    # music/546
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail')
+
 ]
