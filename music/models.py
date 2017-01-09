17662ec5773661442da971c19e96e7e8d7b3abe0
lsjgerardo <lsjgerardo@gmail.com>
2017-01-08 20:26:22 -0600

    init

---
+++ music/models.py
@@ -0,0 +1,20 @@
+
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Album(models.Model):
+    """Album model"""
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=500)
+    album_logo = models.CharField(max_length=1000)
+
+class Song(models.Model):
+    """CASCADE, if the album is deleted the songs too"""
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=10)
+    song_title = models.CharField(max_length=250)
+    
