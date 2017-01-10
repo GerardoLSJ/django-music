793b31d9f0173aecfa8fceb920dc7318c4a5aacc
lsjgerardo <lsjgerardo@gmail.com>
2017-01-09 18:50:59 -0600

    http404

--- music/views.py
+++ music/views.py
@@ -1,6 +1,7 @@
 # pylint: disable=C0111
-from django.http import HttpResponse
+#from django.http import HttpResponse
 #from django.template import loader
+from django.http import Http404
 from django.shortcuts import render
 from .models import Album
 
@@ -16,5 +17,10 @@ def index(request):
     return render(request, 'music/index.html',context)
 
 def detail(request, album_id):
-    return HttpResponse("<h2>Details for Album id: " + str(album_id) + " </h2>")
+    try:
+        album = Album.objects.get(id=album_id)
+    except Album.DoesNotExist:
+        raise Http404('Album does not exist')
+
+    return render(request, 'music/detail.html', {'album':album})
 
