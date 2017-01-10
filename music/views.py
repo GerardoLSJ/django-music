c849af8688519f87b50e498065bd3b7eeec85cc9
lsjgerardo <lsjgerardo@gmail.com>
2017-01-09 18:40:42 -0600

    shortcut

--- music/views.py
+++ music/views.py
@@ -1,18 +1,19 @@
 # pylint: disable=C0111
 from django.http import HttpResponse
-from django.template import loader
+#from django.template import loader
+from django.shortcuts import render
 from .models import Album
 
 def index(request):
     """ return all elements"""
     all_albums = Album.objects.all()
     html = ''
-    template = loader.get_template('music/index.html')
+   #template = loader.get_template('music/index.html')
     context = {
         'all_albums' : all_albums,
 
     }
-    return HttpResponse(template.render(context,request))
+    return render(request, 'music/index.html',context)
 
 def detail(request, album_id):
     return HttpResponse("<h2>Details for Album id: " + str(album_id) + " </h2>")
