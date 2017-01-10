fcb561df52158b7f53511ac002abb84053738479
lsjgerardo <lsjgerardo@gmail.com>
2017-01-09 22:16:32 -0600

    db ops

--- music/models.py
+++ music/models.py
@@ -22,7 +22,12 @@ class Song(models.Model):
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
 
+    def __str__(self):
+        return self.song_title
+
 """
+album1.song_set.create(song_title="Blank space", file_type="mp3")
+
 >>> from music.models import *
 >>> Album.objects.all()
 """
