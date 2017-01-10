77636f3ce545b65871a37ab29e92e1e0f4605b23
lsjgerardo <lsjgerardo@gmail.com>
2017-01-09 18:30:02 -0600

    template

--- music/views.py
+++ music/views.py
@@ -1,7 +1,19 @@
+# pylint: disable=C0111
 from django.http import HttpResponse
+from django.template import loader
+from .models import Album
 
 def index(request):
-    return HttpResponse('<h1>Music app Homepage</h1>')
+    """ return all elements"""
+    all_albums = Album.objects.all()
+    html = ''
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums' : all_albums,
+
+    }
+    return HttpResponse(template.render(context,request))
 
 def detail(request, album_id):
     return HttpResponse("<h2>Details for Album id: " + str(album_id) + " </h2>")
+
