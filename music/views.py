07882c16dca5d186c3515f31781936320facaced
lsjgerardo <lsjgerardo@gmail.com>
2017-01-13 21:32:51 -0600

    http 404 unused

--- music/views.py
+++ music/views.py
@@ -2,8 +2,8 @@
 # pylint: disable=E1101
 #from django.http import HttpResponse
 #from django.template import loader
-from django.http import Http404
-from django.shortcuts import render
+#from django.http import Http404
+from django.shortcuts import render, get_object_or_404
 from .models import Album
 
 def index(request):
@@ -17,10 +17,12 @@ def index(request):
     return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
+    """
     try:
         album = Album.objects.get(id=album_id)
     except Album.DoesNotExist:
         raise Http404('Album does not exist')
-
+    """
+    album = get_object_or_404(Album, id=album_id)
     return render(request, 'music/detail.html', {'album':album})
 
