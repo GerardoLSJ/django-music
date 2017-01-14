911c12ddd15df2c9b44e1c5937a4decd8e0234ec
lsjgerardo <lsjgerardo@gmail.com>
2017-01-13 21:29:34 -0600

    name the urls pattern

--- music/views.py
+++ music/views.py
@@ -1,4 +1,5 @@
 # pylint: disable=C0111
+# pylint: disable=E1101
 #from django.http import HttpResponse
 #from django.template import loader
 from django.http import Http404
@@ -8,13 +9,12 @@ from .models import Album
 def index(request):
     """ return all elements"""
     all_albums = Album.objects.all()
-    html = ''
    #template = loader.get_template('music/index.html')
     context = {
         'all_albums' : all_albums,
 
     }
-    return render(request, 'music/index.html',context)
+    return render(request, 'music/index.html', context)
 
 def detail(request, album_id):
     try:
